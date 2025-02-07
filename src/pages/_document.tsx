9b1ce780ac461663c53a359c59fd19fd38d6a25b
gwonderboy <vebubechukwu@gmail.com>
2025-02-07 17:31:19 +0100

    feat: add max-width

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,19 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <meta name="description" content="From Waste to Wealth" />
           <title>Energy Chleen</title>
+          <style>{`
+            body {
+              max-width: 768px;
+              margin: 0 auto;
+              width: 100%;
+            }
+          `}</style>
         </Head>
         <body>
           <Main />
